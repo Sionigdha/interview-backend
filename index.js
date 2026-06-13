196d95d030015f9bbf4a9ce8926e6960fa390cb9
Sionigdha Sadhukhan <snigdha.lee75@gmail.com>
2026-06-13 15:26:14 +0530

    path fixes

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ const multer = require('multer')
 const path = require('path')
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_PASS }
 })
 
